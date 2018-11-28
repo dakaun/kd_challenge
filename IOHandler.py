7a906da22e4b2c544b212a32b0c3fcdf131190ee
Daniela Kaun <ugebn@student.kit.edu>
2018-11-28 11:02:06 +0100

    split method

--- IOHandler.py
+++ IOHandler.py
@@ -1,1 +1,5 @@
+from sklearn.model_selection import train_test_split
 
+def split_train_test(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
+    return X_train, X_test, y_train, y_test
