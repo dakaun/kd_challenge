24b918411989f319315b663970c54bef916c76e9
Daniela Kaun <ugebn@student.kit.edu>
2018-12-04 09:03:42 +0100

    adapt general methods

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,32 +1,21 @@
 import IOHandler
+import PreprocessingHandler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
+
 
 # extract numerical columns
 def extract_numerical(dataset):
     subset = dataset[['label', 'budget', 'popularity', 'revenue', 'runtime']]
     return subset
 
-def drop_columns(dataset, column_list):
-    for column in column_list:
-        dataset = dataset.drop(column, axis=1)
-    return dataset
-
-def get_X_y(dataset):
-    target = dataset.iloc[:, :1]
-    X = dataset.iloc[:, 1:]
-    return X, target
-
-
 
 data = IOHandler.read_data()
 data = extract_numerical(data)
-#data = drop_columns(data, ['imdbId', 'tmdbId'])
-X, y = get_X_y(data)
-print(type(X))
-print(X)
-X.info()
+# data = drop_columns(data, ['imdbId', 'tmdbId'])
+X, y = PreprocessingHandler.get_X_y(data)
+X_stand = PreprocessingHandler.standard(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 decisiontree = DecisionTreeClassifier()
--- PreprocessingHandler.py
+++ PreprocessingHandler.py
@@ -1,1 +1,24 @@
 # The PreprocessingHandler contains methods for cleaning and preprocessing the data prior to analyzing it.
+
+from sklearn.preprocessing import StandardScaler
+
+
+# give list of columns to drop
+def drop_columns(dataset, column_list):
+    for column in column_list:
+        dataset = dataset.drop(column, axis=1)
+    return dataset
+
+
+# split dataset into data and target
+def get_X_y(dataset):
+    target = dataset.iloc[:, :1]
+    X = dataset.iloc[:, 1:]
+    return X, target
+
+
+# standardize data
+def standard(X):
+    scaler = StandardScaler()
+    X_trans = scaler.fit_transform(X)
+    return X_trans
