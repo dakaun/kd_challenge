01775d38b6ce4848d9070f8bebb83aba343ed100
Julius Landwehr <landwehr.julius@outlook.com>
2019-01-03 11:27:22 +0100

    Nested DT functionality for function call

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,18 +1,29 @@
-import IOHandler
-import PreprocessingHandler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
+
+import PreprocessingHandler as ph
 
 
-data = IOHandler.read_data('num_genre')
-#data = pd.read_csv('./data/num_genre.csv')
-X, y = PreprocessingHandler.get_X_y(data)
-#X_stand = PreprocessingHandler.standard(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+# data = IOHandler.read_data('num_genre')
+# data = pd.read_csv('./data/num_genre.csv')
+# X, y = PreprocessingHandler.get_X_y(data)
+# X_stand = PreprocessingHandler.standard(X)
+# X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-decisiontree = DecisionTreeClassifier()
-decisiontree.fit(X_train, y_train)
-y_pre = decisiontree.predict(X_test)
-dt_score = decisiontree.score(X_test, y_test)
-print(f'Score is : {dt_score}')
+# decisiontree = DecisionTreeClassifier()
+# decisiontree.fit(X_train, y_train)
+# y_pre = decisiontree.predict(X_test)
+# dt_score = decisiontree.score(X_test, y_test)
+# print(f'Score is : {dt_score}')
+
+
+def decision_tree(data):
+    X, y = ph.get_X_y(data)
+    # X_stand = ph.standard(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    decisiontree = DecisionTreeClassifier()
+    decisiontree.fit(X_train, y_train)
+    y_pre = decisiontree.predict(X_test)
+    dt_score = decisiontree.score(X_test, y_test)
+    print(f'Score is : {dt_score}')
