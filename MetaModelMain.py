4d1bb34ff7e9716a9b422352b7c9bb578728f7bb
Johanna von Hammerstein <johannavonhammerstein@gmail.com>
2019-01-23 21:43:54 +0100

    Neue Jozys MainMethode und ein extra skript für das Metamodell, dort kann rumgespielt werden.

--- MetaModelMain.py
+++ MetaModelMain.py
@@ -4,43 +4,16 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-
-import DecisionTree as dt
-from sklearn import tree
-import MLPClassifier as mlp
-import SVMClassifier as svm
-import NearestCentroidClassifier as nc
-import GNBClassifier as gnb
-import NearestNeigborClassification as knn
 from sklearn.naive_bayes import MultinomialNB as bayes
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.feature_extraction.text import CountVectorizer as countvec
 
-# df = io.read_data('train')
-# # # bag of words for overview and title
-# #title_bag=ph.bag_of_words(df['title'])
-# #overview_bag=ph.bag_of_words(df['overview'])
-# #print (title_bag)
-# ph.hot_encode_and_write_all_columns(df,'train')
-# io.write_df(df['label'], 'label_train')
-# label = pd.read_csv('./data/label_train.csv')
-# genres = pd.read_csv('./data/genres_train.csv')
-# keywords = pd.read_csv('./data/keywords_train.csv')
-# production_companies = pd.read_csv('./data/prod_companies_train.csv')
-# production_countries = pd.read_csv('./data/prod_countries_train.csv')
-# #release_date = pd.read_csv('./data/release_date_train.csv')
-# numerical = pd.read_csv('./data/numerical_df_train.csv')
-# spoken_languages = pd.read_csv('./data/spoken_languages_train.csv')
+# bereits probiert: bayes und decision tree für die beiden bag of words
+# todo: mit dem metamodell rumspielen und unterschiedliche kombis und 
+#       classifier ausprobieren
 
-# frames = [label,genres, keywords, production_companies, production_countries, numerical, spoken_languages]
-# total_df = ph.combine_df(frames)
-# io.write_df(total_df, 'total_df')
 
-#X_full=pd.read_csv('./data/total_df.csv')
-#label = X_full.loc[:,'label']
-#X = X_full.loc[:, X_full.columns !='label']
-#bag of words for overview and title
 df = io.read_data('train')
 data=ph.extract_numerical(df) #numerical data
 y=data.loc[:,'label']
@@ -75,7 +48,7 @@ y_trainSub = trainSub['label']
 y_trainMeta = trainMeta['label']
 y_test = test['label']
 
-#bayes für title
+#decision tree für title
 #countvec für title
 countvectorizer_title = countvec()
 x_trainSub_title = countvectorizer_title.fit_transform(trainSub_title)
@@ -92,7 +65,7 @@ print(titleScore_text)
 stacked_input1 = pd.Series(tree_title.predict(x_trainMeta_title))
 stacked_input1_test = pd.Series(tree_title.predict(x_test_title))
 
-#bayes für overview
+#decision tree für overview
 #countvec für overview
 countvectorizer_over = countvec()
 x_trainSub_over = countvectorizer_over.fit_transform(trainSub_over)
@@ -145,62 +118,3 @@ print(titleScore_text)
 print(overScore_text)
 print(numScore_text)
 print(metaScore_text)
-
-#split into training and testing set
-#X_train, X_test, y_train, y_test = train_test_split(X_wText, y)
-
-#pca
-# X_train_p=ph.dim_reduction_components(X_train,10)
-# X_test_p=ph.dim_reduction_components(X_test,10)
-
-# mlp.mlp_classifier(X_train_p, X_test_p, y_train,y_test)
-# svm.svm_classifier(X_train_p, X_test_p, y_train,y_test)
-# nc.nc_classifier(X_train_p, X_test_p, y_train,y_test)
-# gnb.gnb_classifier(X_train_p, X_test_p, y_train,y_test)
-# knn.knn_classifier(X_train_p, X_test_p, y_train,y_test)
-
-
-#X_reduced = pd.DataFrame(ph.dim_reduction_components(X, 10))
-#frames = [X_reduced, label]
-#data_train=ph.combine_df(frames)
-
-# print(X.shape)
-# print(X_reduced.shape)
-
-#d=dt.decision_tree(X_full)
-#tree.export_graphviz(d, out_file='tree.dot')
-
-#mlp.mlp_classifier(X_full)
-#svm.svm_classifier(X_full)
-#nc.nc_classifier(X_full)
-#gnb.gnb_classifier(X_full)
-#knn.knn_classifier(X_full)
-
-
-
-
-
-
-
-
-#genres_reduction = ph.dim_reduction_var_exp(trans_genre_df, 0.8)
-
-# Create DF from ndarray
-#height, width = genres_reduction.shape
-#x = ['Genre_PC_%s' % i for i in range(1, width + 1)]
-#genres_reduction_df = pd.DataFrame(data=genres_reduction, columns=x)
-
-#train_dataset = ph.combine_df([ph.extract_numerical(df_reduced), genres_reduction_df])
-#print(train_dataset)
-
-#dt.decision_tree(train_dataset)
-#mlp.mlp_classifier(train_dataset)
-#svm.svm_classifier(train_dataset)
-#nc.nc_classifier(train_dataset)
-#gnb.gnb_classifier(train_dataset)
-#knn.knn_classifier(train_dataset)
-
-
-
-#prod_countries_reductions = io.read_data('prod_countries_train')
-#ph.dim_reduction_var_exp(prod_countries_reductions, 0.8)
